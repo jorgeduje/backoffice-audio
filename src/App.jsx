2cb164805770dc0018120ebc6ec5a29c62130f31
monimunozalzate <106264010+monimunozalzate@users.noreply.github.com>
2023-05-09 18:06:41 -0500

    anexando themeConfig y custom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./Router/AppRouter";
+import { theme } from "./themeConfig";
+import { ThemeProvider } from "@emotion/react";
 
 function App() {
   return (
     <BrowserRouter>
-      <AppRouter />
+      <ThemeProvider theme={theme}>
+        <AppRouter />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
